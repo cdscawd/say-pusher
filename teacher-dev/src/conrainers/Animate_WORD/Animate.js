05452fff78b613e335009269d0f4bbe4993e8285
dsfsfrec <gatsbychao@gmail.com>
2017-12-20 16:23:13 +0800

    1

--- teacher-dev/src/conrainers/Animate_WORD/Animate.js
+++ teacher-dev/src/conrainers/Animate_WORD/Animate.js
@@ -8,10 +8,12 @@ class Animate extends Component {
 		// 页面展现数据
 		dom_data: {
 			image: [],
-			word: []
+			text: []
 		},
 		// 鼠标点击次数
 		target_number: 0,
+		// 鼠标点击类型
+		target_type: 'IMAGE',		//IMAGE		 
 		// 请求获得数据	
 		data: {
 			background: {
@@ -35,6 +37,57 @@ class Animate extends Component {
 						fontSize: 28,
 						animation: 'animated fadeInUpBig'
 					}
+				}, {
+					image: {
+						path: './animation/step_01/dad.png',
+						width: 200,
+						height: 200,
+						top: 140,
+						left: 280,
+						animation: 'animated fadeInUp'
+					},
+					text: {
+						value: 'dad',
+						top: 340,
+						left: 280,
+						width: 200,
+						fontSize: 28,
+						animation: 'animated fadeInUpBig'
+					}
+				}, {
+					image: {
+						path: './animation/step_01/grandma.png',
+						width: 200,
+						height: 200,
+						top: 140,
+						left: 520,
+						animation: 'animated fadeInDown'
+					},
+					text: {
+						value: 'grandma',
+						top: 340,
+						left: 520,
+						width: 200,
+						fontSize: 28,
+						animation: 'animated fadeInUpBig'
+					}
+				}, {
+					image: {
+						path: './animation/step_01/grandpa.png',
+						width: 200,
+						height: 200,
+						top: 140,
+						left: 760,
+						animation: 'animated fadeInRight'
+					},
+					text: {
+						value: 'grandpa',
+						top: 340,
+						left: 760,
+						width: 200,
+						fontSize: 28,
+						animation: 'animated fadeInUpBig'
+					}
 				}
 			],
 		}
@@ -46,6 +99,7 @@ class Animate extends Component {
 		var target_number = this.state.target_number
 		var dom_data = this.state.dom_data
 		var word = this.state.data.word
+		var target_type = this.state.target_type
 
 		// if (target_number < word.length) {
 		// 	dom_data.push(word[target_number])
@@ -57,12 +111,27 @@ class Animate extends Component {
 		// console.log(target_number)
 		// console.log(this.state.dom_data)
 
-			dom_data.image.push(word[target_number].image)
-			this.setState({
-				dom_data: dom_data
-			})
-			console.log(this.state.dom_data)
-
+		if (target_number < word.length) {
+			if (target_type === 'IMAGE') { 
+				dom_data.image.push(word[target_number].image)
+				this.setState({
+					dom_data: dom_data,
+					target_type: 'WORD'
+				})
+			} else {
+				dom_data.text.push(word[target_number].text)
+				this.setState({
+					dom_data: dom_data,
+					target_type: 'IMAGE',
+					target_number: target_number + 1,
+				})
+			}
+		} else {
+			alert('WORD 已完成练习 跳转下一个')
+		}
+		
+		console.log(this.state.dom_data)
+		// 处理文字推入深渊
 	}
 	render() {
 		var data = this.state.data
@@ -70,13 +139,6 @@ class Animate extends Component {
 
 		return (
 			<section id="Animate-content" style={{backgroundImage: data.background.image_path}}>
-
-				{/*<img  className={`word-image ${words[0].image.animation}`} style={{
-					width: words[0].image.width,
-					height: words[0].image.height,
-					left: words[0].image.left,
-					top: words[0].image.top,
-				}} src={words[0].image.path} />*/}
 
 				{
 					this.state.dom_data.image.map((item, index) => {
@@ -86,6 +148,17 @@ class Animate extends Component {
 													left: item.left,
 													top: item.top,
 												}} src={item.path} />
+					})
+				}
+				{
+					this.state.dom_data.text.map((item, index) => {
+						return  <span key={index} className={`word-text ${item.animation}`} style={{
+											width: item.width,
+											top: item.top,
+											left: item.left, 
+											color: item.color,
+											fontSize: item.fontSize,
+										}}>{item.value}</span>
 					})
 				}
 
