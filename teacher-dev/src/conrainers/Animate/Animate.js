03d85bd43e0075ef1b8c559fc95ee5705c63a215
dsfsfrec <gatsbychao@gmail.com>
2017-12-20 11:06:37 +0800

    1

--- teacher-dev/src/conrainers/Animate/Animate.js
+++ teacher-dev/src/conrainers/Animate/Animate.js
@@ -5,6 +5,13 @@ import pic_01 from '../../images/pic_01.png'
 
 class Animate extends Component {
 	state = {
+		// 页面展现数据
+		dom_data: [],
+
+		// 鼠标点击次数
+		target_number: 0,
+
+		// 请求获得数据	
 		data: {
 			background: {
 				image_path: "url('./animation/step_01/bg.png')"
@@ -24,14 +31,7 @@ class Animate extends Component {
 						top: 340,
 						left: 40,
 						width: 200,
-						color: '#000',
-						fontFamily: 'Comic Sans MS',
 						fontSize: 28,
-						fontStyle: 'normal',
-						fontWeight: '400',
-						textAlign: 'center',
-						textDecoration: 'none',
-						whiteSpace: 'nowrap',
 						animation: 'animated fadeInUpBig'
 					}
 				}, {
@@ -41,21 +41,14 @@ class Animate extends Component {
 						height: 200,
 						top: 140,
 						left: 280,
-						animation: 'animated fadeInLeft'
+						animation: 'animated fadeInUp'
 					},
 					text: {
 						value: 'dad',
 						top: 340,
 						left: 280,
 						width: 200,
-						color: '#000',
-						fontFamily: 'Comic Sans MS',
 						fontSize: 28,
-						fontStyle: 'normal',
-						fontWeight: '400',
-						textAlign: 'center',
-						textDecoration: 'none',
-						whiteSpace: 'nowrap',
 						animation: 'animated fadeInUpBig'
 					}
 				}, {
@@ -65,21 +58,14 @@ class Animate extends Component {
 						height: 200,
 						top: 140,
 						left: 520,
-						animation: 'animated fadeInLeft'
+						animation: 'animated fadeInDown'
 					},
 					text: {
 						value: 'grandma',
 						top: 340,
 						left: 520,
 						width: 200,
-						color: '#000',
-						fontFamily: 'Comic Sans MS',
 						fontSize: 28,
-						fontStyle: 'normal',
-						fontWeight: '400',
-						textAlign: 'center',
-						textDecoration: 'none',
-						whiteSpace: 'nowrap',
 						animation: 'animated fadeInUpBig'
 					}
 				}, {
@@ -89,33 +75,42 @@ class Animate extends Component {
 						height: 200,
 						top: 140,
 						left: 760,
-						animation: 'animated fadeInLeft'
+						animation: 'animated fadeInRight'
 					},
 					text: {
 						value: 'grandpa',
 						top: 340,
 						left: 760,
 						width: 200,
-						color: '#000',
-						fontFamily: 'Comic Sans MS',
 						fontSize: 28,
-						fontStyle: 'normal',
-						fontWeight: '400',
-						textAlign: 'center',
-						textDecoration: 'none',
-						whiteSpace: 'nowrap',
 						animation: 'animated fadeInUpBig'
 					}
 				}
 			],
 		}
+
 	};
 	componentWillMount() {}
-	componentDidMount() {}
+	componentDidMount() {
+		console.log(this.state.data.word.length)
+	}
 	componentWillUnmount() {}
-
 	nextButtonBind = () => {
-		console.log($('.word-text').text())
+		var target_number = this.state.target_number
+		var dom_data = this.state.dom_data
+		var word = this.state.data.word
+
+		if (target_number < word.length) {
+			dom_data.push(word[target_number])
+			this.setState({
+				target_number: target_number + 1,
+				dom_data: dom_data
+			})
+		}
+
+		// console.log(target_number)
+		// console.log(this.state.dom_data)
+
 	}
 	render() {
 		var data = this.state.data
@@ -124,26 +119,32 @@ class Animate extends Component {
 		return (
 			<section id="Animate-content" style={{backgroundImage: data.background.image_path}}>
 
-				<img  className={`word-image ${words[0].image.animation}`} style={{
+				{/*<img  className={`word-image ${words[0].image.animation}`} style={{
 					width: words[0].image.width,
 					height: words[0].image.height,
 					left: words[0].image.left,
 					top: words[0].image.top,
-				}} src={words[0].image.path} />
+				}} src={words[0].image.path} />*/}
 
-				<span className={`word-text ${words[0].text.animation}`} style={{
-					width: words[0].text.width,
-					top: words[0].text.top,
-					left: words[0].text.left, 
-					color: words[0].text.color,
-					fontFamily: words[0].text.fontFamily,
-					fontSize: words[0].text.fontSize,
-					fontStyle: words[0].text.fontStyle,
-					fontWeight: words[0].text.fontWeight,
-					textAlign: words[0].text.textAlign,
-					textDecoration: words[0].text.textDecoration,
-					whiteSpace: words[0].text.whiteSpace,
-				}}>{words[0].text.value}</span>
+				{
+					this.state.dom_data.map((item, index) => {
+						return  <div key={index}>
+											<img className={`word-image ${item.image.animation}`} style={{
+													width: item.image.width,
+													height: item.image.height,
+													left: item.image.left,
+													top: item.image.top,
+												}} src={item.image.path} />
+											<span className={`word-text ${item.text.animation}`} style={{
+													width: item.text.width,
+													top: item.text.top,
+													left: item.text.left, 
+													color: item.text.color,
+													fontSize: item.text.fontSize,
+												}}>{item.text.value}</span>
+										</div>
+					})
+				}
 
 				<div className="control-tools">
 					<div className="next" title="next action" 
