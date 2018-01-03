68452b456d03fc7ab5de993de98a3458d8596c7d
dsfsfrec <gatsbychao@gmail.com>
2018-01-03 15:36:41 +0800

    1

---
+++ VoIP-Subject/READEME.md
@@ -0,0 +1,11 @@
+
+# 课程资源服务
+
+## Start
+```
+  $ npm i
+  $ npm run start
+```
+
+#About
+ Subject server port is 8002 (/bin/www)
--- VoIP-Subject/app.js
+++ VoIP-Subject/app.js
@@ -1,14 +1,11 @@
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var index = require('./routes/index');
-var sessionend = require('./routes/sessionend');
 
 var app = express();
 
@@ -17,18 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-// https://expressjs.com/zh-cn/starter/static-files.html
-app.use(favicon(path.join(__dirname, 'public/images', 'favicon.png')));
-app.use(logger('dev')); //日志中间件
-app.use(bodyParser.json()); //解析json的中间件
-app.use(bodyParser.urlencoded({ extended: false }));  //解析urlencoded请求体的中间件
-app.use(cookieParser());  //解析cookie的中间件
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use('/static', express.static(__dirname + '/public'));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('files'));
 
-app.use('/teacher', index);
-app.use('/sessionend', sessionend);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,6 +40,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 module.exports = app;
