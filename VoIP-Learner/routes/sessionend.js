4aba2a0bb70663ec8eb35bec4f5b5c0430a3988b
dsfsfrec <gatsbychao@gmail.com>
2018-01-13 12:22:31 +0800

    1

--- VoIP-Learner/routes/sessionend.js
+++ VoIP-Learner/routes/sessionend.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET sessionend. */
 router.get('/', function(req, res, next) {
-  res.send('<p style="text-align: center;">会话已经结束。</p>');
+  res.send('<div style="height: 100%;display: flex;justify-content: center;align-items: center;"><p style="">会话已经结束。</p></div>');
 });
 
 module.exports = router;
--- VoIP-Teacher/routes/sessionend.js
+++ VoIP-Teacher/routes/sessionend.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET sessionend. */
 router.get('/', function(req, res, next) {
-  res.send('<p style="text-align: center;">Session was end.</p>');
+  res.send('<div style="height: 100%;display: flex;justify-content: center;align-items: center;"><p style="font-size: 24px;">Session was end.</p></div>');
 });
 
 module.exports = router;
